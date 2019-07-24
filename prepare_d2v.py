50b24fb5cdfc800ffa5a6ac78704b4f6aafa5b2d
1001255 <khkoo@sk.com>
2019-07-25 01:35:34 +0900

    use keywords only

--- prepare_d2v.py
+++ prepare_d2v.py
@@ -1,20 +1,15 @@
 #-*- encoding: utf-8 -*-
-# prepration process for doc2vec
-# write article_sentences.txt with all text of articles
+# preprare sentence for doc2vec
 # write writer_user_sentences.txt with keywords of writer/user
 
 import os, sys 
 import pdb 
 import json
 import tqdm
-from konlpy.tag import Mecab
 from util import iterate_data_files
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 # may need to write pickle for this
 def read_reads():
-  print("read reads for the test set")
   files = sorted([path for path, _ in iterate_data_files('2018100100', '2019030100')])
   for path in tqdm.tqdm(files, mininterval=1):
     date = path[11:19]
@@ -44,43 +39,29 @@ def read_reads():
             reads_dup[user_id] = reads
 
 def read_test_user():
-  print("read test user set", user_file)
   with open(user_file, "r") as fp:
     for line in fp:
       viewer_id = line.strip()
       t_users[viewer_id] = 1
 
-def normalize_mecab(line):
-  tokens = mecab.morphs(line)
-  sentence = " ".join(tokens)
-  return sentence
-
 if __name__ == "__main__":
-  if sys.argv[1] == "test":
-    user_file = "res/predict/test.users"
-  elif sys.argv[1] == "dev":
-    user_file = "res/predict/dev.users"
-  else:
-    sys.exit()
 
+  user_file = "res/predict/test.users"
   print("read test users")
   t_users = {}
   read_test_user()
 
-  print("read reads of test users")
+  print("read reads of all users")
   t_reads = {}
   t_reads_dup = {}
   o_reads = {}
   reads_dup = {}
   read_reads()
 
-  mecab = Mecab()
   articles = {}
   writer_articles = {}
 
-  print("read articles and write article sentences")
-  of1 = open("article_sentences.txt", "w")
-  of2 = open("titles.txt", "w")
+  print("read articles metadata")
   with open("res/metadata.json", "r") as fp:
     for line in fp:
       article = json.loads(line)
@@ -88,52 +69,31 @@ if __name__ == "__main__":
       writer_id = article['user_id']
       title = article['title'].strip()
       sub_title = article['sub_title'].strip()
-      n_title = normalize_mecab(title)
-      of2.write(n_title + "\n")
-      of1.write(article_id + "\t" + n_title + "\n")
-      if sub_title != "":
-        n_sub_title = normalize_mecab(sub_title)
-        of2.write(n_sub_title + "\n")
-        of1.write(article_id + "\t" + n_sub_title + "\n")
-      else:
-        n_sub_title = ""
       keywords = " ".join(article['keyword_list']).strip()
-      if keywords != "":
-        n_keywords = normalize_mecab(keywords)
-        of1.write(article_id + "\t" + n_keywords + "\n")
-      else:
-        n_keywords = ""
-      articles[article_id] = [n_title, n_sub_title, n_keywords]
+      articles[article_id] = [title, sub_title, keywords]
       if writer_id in writer_articles:
         writer_articles[writer_id].append(article_id)
       else:
         writer_articles[writer_id] = [article_id]
-  of1.close()
-  of2.close()
 
+  print("write writer sentences")
   num_writer = 0
   num_write_article = 0
-  of3 = open("writer_user_sentences.txt", "w")
-  print("write writer/test_user sentences")
+  of3 = open("res/writer_user_sentences_keyword.txt", "w")
   for writer in writer_articles:
-    of3.write(writer)
     num_writer += 1
-    for article in writer_articles[writer]:
+    line = ""
+    recent_articles = writer_articles[writer]
+    for article in recent_articles:
       num_write_article += 1
       title = articles[article][0]
       sub_title = articles[article][1]
       keywords = articles[article][2]
       if keywords != "":
-        #of3.write(writer + "\t" + keywords + "\n")
-        of3.write(" " + keywords)
-      elif sub_title != "":
-        #of3.write(writer + "\t" + sub_title + "\n")
-        of3.write(" " + sub_title)
-      else:
-        #of3.write(writer + "\t" + title + "\n")
-        of3.write(" " + title)
-    of3.write("\n")
+        line += " " + keywords
+    if line != "": of3.write(writer + line + "\n")
 
+  print("write user sentences")
   num_user = 0
   num_read_article = 0
   for user in t_users:
@@ -142,54 +102,36 @@ if __name__ == "__main__":
       reads = set(t_reads[user])
     if len(reads) < 1: continue
     num_user += 1
-    of3.write(user)
+    line = ""
     for article in reads:
       if article in articles:
         title = articles[article][0]
         sub_title = articles[article][1]
         keywords = articles[article][2]
         if keywords != "":
-          #of3.write(user + "\t" + keywords + "\n")
-          of3.write(" " + keywords)
-        elif sub_title != "":
-          #of3.write(user + "\t" + sub_title + "\n")
-          of3.write(" " + sub_title)
-        else:
-          #of3.write(user + "\t" + title + "\n")
-          of3.write(" " + title)
+          line += " " + keywords
         num_read_article += 1
-      else:
-        print("article not found in meta", article)
-    of3.write("\n")
 
-  num_user = 0
-  num_read_article = 0
+    if line != "": of3.write(user + line + "\n")
+
   for user in reads_dup:
-    if num_user > 20000: break
+    if num_user > 14828: break
     reads = set(reads_dup[user])
     if len(reads) < 20:
       reads = set(o_reads[user])
     if len(reads) < 1: continue
     num_user += 1
-    of3.write(user)
+    line = ""
     for article in reads:
       if article in articles:
         title = articles[article][0]
         sub_title = articles[article][1]
         keywords = articles[article][2]
         if keywords != "":
-          #of3.write(user + "\t" + keywords + "\n")
-          of3.write(" " + keywords)
-        elif sub_title != "":
-          #of3.write(user + "\t" + sub_title + "\n")
-          of3.write(" " + sub_title)
-        else:
-          #of3.write(user + "\t" + title + "\n")
-          of3.write(" " + title)
+          line += " " + keywords
         num_read_article += 1
-      else:
-        print("article not found in meta", article)
-    of3.write("\n")
+    if line != "": of3.write(user + line + "\n")
 
   of3.close()
-  print(str(num_writer), str(num_write_article), str(num_user), str(num_read_article))
+  #print(str(num_writer), str(num_write_article), str(num_user), str(num_read_article))
+  print("all done!")
